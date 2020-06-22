1528ce4119257124732f0fcfd2aaaf53bfbfef1f
MasonWang025 <mason.wang0025@gmail.com>
2020-06-22 12:47:47 -0700

    push

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function About() {
   return (
     <div className="mt-5 pt-5 text-center">
-      This page is currrently being updated. We apologize for any inconveniences.
+      This page is currently being updated. We apologize for any inconveniences.
       <div className="col-md-10 mx-auto mb-3 mt-4 text-center mt-3">
         <Link className="btn-get-started main-action text-decoration-none">
           Contact Coco
